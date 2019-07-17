60456ffa66f6bd432762017ee7086b9c452e14e4
huang054 <huangjun054@163.com>
2019-07-17 15:26:14 +0800

    commit

--- src/main/java/com/client/springbootcloudclient/environment/MyEnvironment.java
+++ src/main/java/com/client/springbootcloudclient/environment/MyEnvironment.java
@@ -4,10 +4,7 @@ import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,12 +14,33 @@ import java.util.Map;
 public class MyEnvironment  implements PropertySourceLocator {
     @Override
     public PropertySource<?> locate(Environment environment) {
+     /*   if (environment instanceof ConfigurableEnvironment) {
+             //两种方式一
+            ConfigurableEnvironment configurableEnvironment = ConfigurableEnvironment.class.cast(environment);
+
+            // 获取 PropertySources
+            MutablePropertySources propertySources = configurableEnvironment.getPropertySources();
+            // 定义一个新的 PropertySource，并且放置在首位
+            propertySources.addFirst(createPropertySource());
+
+        }*/
+     //二
         Map<String,Object> map = new HashMap<>();
         //以9000端口启动
         map.put("server.port",9000);
         MapPropertySource propertySource = new MapPropertySource("mydemo",map);
         return propertySource;
     }
+    private PropertySource createPropertySource() {
 
+        Map<String, Object> source = new HashMap<>();
+
+        source.put("spring.application.name", " Spring Cloud 程序");
+        // 设置名称和来源
+        PropertySource propertySource = new MapPropertySource("over-bootstrap-property-source", source);
+
+        return propertySource;
+
+    }
 
 }
